86ae392a9cfc6510624fa586af21b3e3e5170dc8
mjkvasconcelos-eng <mjk.vasconcelos@gmail.com>
2026-09-15 09:45:09 -0300

    Configura Firebase Web do projeto Natura

---
+++ firebase/firebase-config.js
@@ -0,0 +1,11 @@
+// Configuração pública do Firebase Web.
+// Estas credenciais identificam o aplicativo Firebase; regras de acesso ficam no Firestore/Auth.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyKed3ooUWiee6nOdOKeS5BOJZCOimVWo",
+  authDomain: "natura-a22fe.firebaseapp.com",
+  projectId: "natura-a22fe",
+  storageBucket: "natura-a22fe.firebasestorage.app",
+  messagingSenderId: "503067454388",
+  appId: "1:503067454388:web:7a2c30d8fda4a2501260e1",
+  measurementId: "G-GWS8F967P2"
+};
